61d2f812daad505760d52af98c52a9e18d9f7bd4
Nicolas Duchastel de Montrouge <nduchast@hotmail.com>
2015-10-14 07:02:25 -0700

    now node.js code also serves static files

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ var express = require('express'),
 
 var app = express();
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-});
+app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+app.use(express.bodyParser());
 
 app.post('/voter', vote.createKeysAndVote);
 app.post('/voter/:id/certify', vote.certify);
 app.get('/voter/:id', vote.fetchVoter);
+app.use(express.static(__dirname + '/web'));
 
-app.listen(3001);
-console.log('Listening on port 3001...');
+var server = app.listen(80, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('listening at http://%s:%s', host, port);
+});
+
+
